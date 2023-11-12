de8fa3dee70b020bc467deaf3fb0752fc9d3acfc
Shauryacious <shauryaagrawal2718@gmail.com>
2023-11-12 23:58:26 +0530

    Added morgan & did set some parsing middleware

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 //ROUTERS
 const userRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//express.json(), it's specifically about converting the incoming JSON-formatted data into a JavaScript object.
+//So, when you use app.use(express.json()), you are telling your Express application to automatically handle the process of extracting the JSON data from incoming requests, parse it into a JavaScript object, and make that object available in the req.body property. This req.body object can then be used in your route handlers or middleware to access and manipulate the data sent by the client.
+app.use(express.json());
+
+//Will parse the URL Encoded data into the req.body object
+app.use(express.urlencoded({extended: false}));
 
 //Routes
 app.use('/', viewRouter);
