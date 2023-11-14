1ea0835e623da392b67eee4301ae70f9dc70e97e
karanmanglani <bkn45for@gmail.com>
2023-11-14 23:07:41 +0530

    added function for protected route for admin

--- backEnd/controllers/authController.js
+++ backEnd/controllers/authController.js
@@ -113,6 +113,51 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+// Protect route for admin
+// Define a middleware function to check if the user is an admin
+exports.restrictToAdmin = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
+  }
+
+  // Check if the user is an admin
+  if (currentUseruser.role !== "admin") {
+    return next(
+      new AppError("You do not have permission to perform this action.", 403)
+    );
+  }
+
+  // If the user is an admin, grant access to the protected route
+  next();
+});
+
 // Only for rendered pages, no errors!
 exports.isLoggedIn = async (req, res, next) => {
   console.log("running fine");
